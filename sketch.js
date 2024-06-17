99e90473221dafe6159ea71b6b11239111d9331a
João Diniz <116961614+joaodiniz20@users.noreply.github.com>
2024-06-17 17:18:17 -0300

    Update sketch.js
    codigo de movimentação no limites

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 //player
+var DEBUG = true
 var player_position_x;
 var player_position_y;
 var tamanho_player = 50;
 var velocidade_player = 10;
+
+var player_position_mapa_x;
+var player_position_mapa_y;
 //mapa
 let mapa;
 var mapa_position_x = 0;
@@ -11,6 +15,9 @@ var tamanho_mapa = 4096;
 
 var centro_x
 var centro_y
+
+var dist_borda_esquerda;
+var dist_borda_direita;
 
 function preload(){
   mapa = loadImage("assets/Mapa.png");
@@ -21,34 +28,58 @@ function setup() {
   centro_x = width/2-(tamanho_player/2);
   centro_y = height/2-(tamanho_player/2);
   
-  player_position_x = centro_x;
-  player_position_y = centro_y;
+  player_position_x = player_position_mapa_x = centro_x;
+  player_position_y = player_position_mapa_y = centro_y;
+  
+  calc_dist_borda_esquerda()
+  calc_dist_borda_direita()
+  
 }
 
 function draw() {
   //background(220);
   image(mapa,mapa_position_x,mapa_position_y);
-  square(player_position_x, player_position_y, tamanho_player);
+  fill("white")
+  square((player_position_x-(tamanho_player/2)), (player_position_y-(tamanho_player/2)), tamanho_player);
+  fill("black")
   text("Tecla: "+keyCode,50,50);
   text("Centro x: "+centro_x,50,65)
+  text("Distancia borda esquerda ("+player_position_mapa_x+"): "+ dist_borda_esquerda, 50,80);
+  text("Distancia borda direita ("+tamanho_mapa+"-"+player_position_mapa_x+"): "+ dist_borda_direita, 50, 95)
   
   
-  movimento_mapa_x();
-  movimento_mapa_y();
+  calc_dist_borda_esquerda()
+  calc_dist_borda_direita()
+  //movimento_mapa_y();
+  //movimento_player_x()
+  if(((player_position_x -mapa_position_x) <= centro_x) || ((player_position_x -mapa_position_x + tamanho_mapa) <= centro_x)){
+    movimento_player_x();
+  } else {
+    movimento_mapa_x();
+  }
+  
+  fill("red")
+  circle(centro_x,centro_y,10)
+  fill("blue")
+  circle(player_position_x,player_position_y,10)
 }
 
 function movimento_player_x(){
   //Faz player se mecher no x
   if(keyIsDown(39)){
-    player_position_x += velocidade_player;          
+    player_position_x += velocidade_player;
+    player_position_mapa_x += velocidade_player;
   } else if(keyIsDown(37)){
     player_position_x -= velocidade_player;
+    player_position_mapa_x -= velocidade_player;
   }
   //Limita player a tela no x
   if((player_position_x+tamanho_player) >= width){
     player_position_x = width-tamanho_player;
+    player_position_mapa_x = width-tamanho_player;
   } else if(player_position_x <= 0){
     player_position_x = 0;
+    player_position_mapa_x = 0;
   }
 }
 
@@ -68,23 +99,30 @@ function movimento_player_y(){
 }
 
 function movimento_mapa_x(){
-  //move mapa no x
-  if(keyIsDown(39)){
-    mapa_position_x -= velocidade_player;          
-  } else if(keyIsDown(37)){
+  alinha_centro();
+  if(keyIsDown(37)){
     mapa_position_x += velocidade_player;
+    player_position_mapa_x -= velocidade_player;
   }
   //para o mapa no x
-  if(mapa_position_x >= 0){
+  if(mapa_position_x >= 0){ //esquerda
     mapa_position_x = 0;
-  } else if((mapa_position_x+tamanho_mapa) <= width){
+    player_position_mapa_x = 0;
+  } else if((mapa_position_x+tamanho_mapa) <= width){//direita
+    console.log("limita direita")
     mapa_position_x = width-tamanho_mapa;
+    //player_position_mapa_x = width-tamanho_mapa;
+  }else //move mapa no x
+  if(keyIsDown(39) || DEBUG){
+    mapa_position_x -= velocidade_player;
+    player_position_mapa_x += velocidade_player;
+    console.log("tecla direita\n")
   }
 }
 
 function movimento_mapa_y(){
   //movimenta mapa no y
-  if(keyIsDown(38)){
+  if(keyIsDown(38) || DEBUG){
     mapa_position_y += velocidade_player;
   } else if(keyIsDown(40)){
     mapa_position_y -= velocidade_player;
@@ -95,4 +133,19 @@ function movimento_mapa_y(){
   } else if((mapa_position_y+tamanho_mapa) <= height){
     mapa_position_y = height-tamanho_mapa;
   }
+}
+
+function alinha_centro(){
+  if(player_position_x != centro_x){
+    player_position_x = centro_x;
+    player_position_mapa_x = centro_x;
+  }
+}
+
+function calc_dist_borda_esquerda(){
+  dist_borda_esquerda = player_position_mapa_x;
+}
+
+function calc_dist_borda_direita(){
+  dist_borda_direita = tamanho_mapa - player_position_mapa_x;
 }
